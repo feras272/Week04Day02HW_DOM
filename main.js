2011232421dd474e0ecd56ed5cb600802407879f
feras alnowiser <ferasdev@gmail.com>
2022-08-23 14:19:00 +0300

    adding and testing the function when login button is clicked

--- main.js
+++ main.js
@@ -6,4 +6,10 @@ let btn_login = document.querySelector('#login');
 let email_input = document.querySelector('#email_input');
 let password_input = document.querySelector('#password_input')
 
-const users = [ {user:'Feras', password:'1234'}, {user:'Alnowiser', password: '5678'}]
+const users = [ {user:'Feras', password:'1234'}, {user:'Alnowiser', password: '5678'}];
+
+function verify() {
+   console.log('Verify Function')
+}
+
+btn_login.addEventListener('click', verify);
