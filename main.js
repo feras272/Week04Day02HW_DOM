4b543b7108a3fb6d8ccba5f1ff22450c098b7e56
feras alnowiser <ferasdev@gmail.com>
2022-08-23 12:45:22 +0300

    adding divs and IDs in index.html and save the elements in variables within main.js

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 console.log('Tuwaiq Academy JS')
 console.log('Week 4 Day 2 HW')
+
+let paragraph = document.querySelector('#status');
+let btn_login = document.querySelector('#login');
+let email_input = document.querySelector('#email_input');
+let password_input = document.querySelector('#password_input')
